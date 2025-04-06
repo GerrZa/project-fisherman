cb379131838b79ea29cbc01bd98c6a7ed6a77943
GerrZa <thanapatr.ger@gmail.com>
2025-04-07 01:06:59 +0700

    fixing water and ground state changing

--- scr/player/player.gd
+++ scr/player/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 var input = Vector2.ZERO
 var w_input = Vector2.ZERO
 
+#NOTE
+#Every level-based variable will be init as level 1 and be loaded when i impliment level system
+
 #Movement Variable
 const G_GRV = 10
 const G_SPD = 70
@@ -11,10 +14,22 @@ const J_FORCE = 200
 const W_GRV = 2
 var w_lerp = 0.05
 var w_spd = 40
+var w_dash_f = 500
+var curr_dash_t = 0
+const w_dash_t = 1 #how long will dash last before speed decrease
+
+#Oxygen / stamina
+var ox_max = 100
+var curr_ox = 0
 
 var grounded = false
 
 @onready var map = get_tree().current_scene
+
+func _ready() -> void:
+	#init var
+	load_data()
+	var_setup()
 
 func _process(delta: float) -> void:
 	#update var
@@ -23,8 +38,22 @@ func _process(delta: float) -> void:
 	input.y = Input.get_axis("up","down")
 	w_input = input.normalized()
 	
-	#print($StateMachine.state.name)
+	print(input)
 	
 	#multi state transition
-	if global_position.y > map.water_level:
+	if global_position.y > map.water_level and str($StateMachine.state.name[0]) == "g":
 		$StateMachine.tran("w_idle")
+	elif global_position.y <= map.water_level and str($StateMachine.state.name[0]) == "w":
+		if grounded:
+			if input != Vector2.ZERO:
+				$StateMachine.tran("g_walk")
+			else:
+				$StateMachine.tran("g_idle")
+		else:
+			$StateMachine.tran("g_air")
+
+func var_setup():
+	curr_ox = ox_max
+
+func load_data():
+	pass
--- scr/player/state/w_move.gd
+++ scr/player/state/w_move.gd
@@ -11,13 +11,25 @@ func start(msg:={}):
 	pass
 
 func physics_process(dt):
-	plr.velocity = lerp(plr.velocity, plr.w_input * plr.w_spd, plr.w_lerp)
+	if plr.curr_dash_t <= 0:
+		plr.velocity = lerp(plr.velocity, plr.w_input * plr.w_spd, plr.w_lerp)
 	
 	plr.move_and_slide()
+	
+	plr.curr_dash_t -= dt
 
 func process(dt):
-	if plr.input == Vector2.ZERO:
-		fsm.tran("w_idle")
+	#if plr.input == Vector2.ZERO and plr.curr_dash_t <= 0:
+		#fsm.tran("w_idle")
+	
+	if Input.is_action_just_pressed("m2"):
+		plr.curr_dash_t = plr.w_dash_t
+		dash()
 
 func exit(msg:={}):
 	pass
+
+func dash():
+	print("dash")
+	plr.velocity = plr.w_input * plr.w_dash_f
+	
